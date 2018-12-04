c7711020a0f06d51132ce2d62efded230a19b95a
illbethebest <183551053@qq.com>
2018-12-04 10:43:52 +0800

    Update Extented Euclidean Algorithm.c

--- Extented Euclidean Algorithm.c
+++ Extented Euclidean Algorithm.c
@@ -4,40 +4,41 @@
 
 #include "stdafx.h"
 
-int exgcd(int a, int b, int &x, int &y); 
+int exgcd(int Coprime_Number1, int Coprime_Number2, int &Inverse_Of_X, int &Inverse_Of_Y); 
 
 int main()
 {
-	int a,b,x,y;   //x,y用来存放ax+by=1(等价于ax≡1 mod b)的解。
+	int Coprime_Number1,Coprime_Number2；
+	int Inverse_Of_X,Inverse_Of_Y;   //用来存放ax+by=1(等价于ax≡1 mod b)的解。
 	printf("请输入两个正整数：\n");
-	scanf_s("%d%d", &a, &b);    
-	int r=exgcd(a, b, x, y);    
-	printf("%d和%d的最大公约数是：%d\n", a, b, r);    //a,b的值没有发生改变。
-	while (x < 0)	
+	scanf_s("%d%d", &Coprime_Number1, &Coprime_Number2);    
+	int Greatest_Common_Divisor=exgcd(Coprime_Number1, Coprime_Number2, Inverse_Of_X, Inverse_Of_Y);    
+	printf("%d和%d的最大公约数是：%d\n", Coprime_Number1, Coprime_Number2, Greatest_Common_Divisor);   
+	while (Inverse_Of_X < 0)	
 	{
-		x += b;	//输出正整数解。
+		Inverse_Of_X += Coprime_Number2;	//输出正整数解。
 	}
-	y = (1 - a * x) / b;
-	printf("%d和%d构成的贝祖等式的一组整数解为：%d,%d", a, b, x, y); 
+	Inverse_Of_Y = (1 - Coprime_Number1 * Inverse_Of_X) / Coprime_Number2;
+	printf("%d和%d构成的贝祖等式的一组整数解为：%d,%d", Coprime_Number1, Coprime_Number2, Inverse_Of_X, Inverse_Of_Y); 
     return 0;
 }
 
 /*
 函数说明：运用扩展欧几里得算法，计算最大公约数和乘法逆元。
 */
-int exgcd(int a, int b, int &x, int &y)	//注意&x,&y传入的是x,y的地址。
+int exgcd(int Coprime_Number1, int Coprime_Number2, int &Inverse_Of_X, int &Inverse_Of_Y)	//注意传入的是地址。
 {
-	if (b == 0)     //gcd(a,0)=a。
+	if (Coprime_Number2 == 0)     //gcd(Coprime_Number1,0)=Coprime_Number1。
 	{
-		x = 1;
-		y = 0;
-		return a;    //此时a是最大公约数。
+		Inverse_Of_X = 1;
+		Inverse_Of_Y = 0;
+		return Coprime_Number1;    //此时Coprime_Number1是最大公约数。
 	}
 
-	int r = exgcd(b, a%b, x, y);      
-	int t = y;
-		y = x - (a / b) * y;
-		x = t;
+	int Greatest_Common_Divisor = exgcd(Coprime_Number2, Coprime_Number1%Coprime_Number2, Inverse_Of_X, Inverse_Of_Y);      
+	int Middle_Number = Inverse_Of_Y;
+		Inverse_Of_Y = Inverse_Of_X - (Coprime_Number1 / Coprime_Number2) * Inverse_Of_Y;
+		Inverse_Of_X = Middle_Number;
 
-	return r;   //r的值在回调时不会改变。
+	return Greatest_Common_Divisor;   //Greatest_Common_Divisor的值在回调时不会改变。
 }
